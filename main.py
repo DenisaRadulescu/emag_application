b25a37842b5ce034dcf87a735b1963c8aebe5ef8
DenisaRadulescu <denisa.r95@gmail.com>
2024-05-22 20:28:39 +0300

    database created, home, login html templates added

---
+++ main.py
@@ -0,0 +1,42 @@
+
+from flask import Flask, render_template, request, url_for, redirect
+
+import emag_db
+
+app = Flask(__name__)
+
+config = emag_db.read_config()
+user_id, username, password = emag_db.read_admins(config)
+
+
+users = {
+    username: password
+}
+
+@app.route("/")
+def first_function():
+    print(" S-a rulat cand apasam pe link")
+    return render_template("login.html")
+
+
+@app.route("/test")
+def second_function():
+    print(" S-a rulat cand apasam pe link")
+    return render_template("test.html")
+
+
+@app.route("/login", methods=["POST"])
+def web_login():
+    user = request.form['username']
+    passwrd = request.form['password']
+
+    if user in users.keys():
+        if passwrd == users[user]:
+            data = emag_db.read_products(config)
+            return render_template("home.html", data=data)
+
+    return render_template("login.html")
+
+
+if __name__ == '__main__':
+    app.run()
